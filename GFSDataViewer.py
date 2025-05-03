a9d28baae2c78d91ce90505f2d9539cbcf57504f
Aniketh Rao <anikethsrao@gmail.com>
2025-05-03 18:26:03 -0400

    fixed file name in .py

--- GFSDataViewer.py
+++ GFSDataViewer.py
@@ -8,7 +8,7 @@ st.title("GFS Data Viewer")
 
 try:
     # Load CSV
-    df = pd.read_csv("merged.csv")
+    df = pd.read_csv("Merged.csv")
     st.success("CSV loaded successfully!")
 
     # Avoid displaying available columns
